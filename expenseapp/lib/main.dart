d1f07f1f4b57a30bfa5c00f98ac18663023f1f4c
David <choonghumyon@gmail.com>
2019-08-08 21:00:49 +0900

    98 time 3:15 added date and chart funtionality

--- expenseapp/lib/main.dart
+++ expenseapp/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:expenseapp/widgets/transaction_list.dart';
-import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
+
 import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
+import './widgets/chart.dart';
 import './models/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -59,6 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    });
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -102,14 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(),
             TransactionList(_userTransactions),
           ],
         ),
--- expenseapp/lib/widgets/chart.dart
+++ expenseapp/lib/widgets/chart.dart
@@ -1,8 +1,35 @@
+import '../models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+
   List<Map<String, Object>> get groupedTransactionValues {
-    return List.generate(7, (index) {});
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day && 
+            recentTransactions[i].date.month == weekDay.month && 
+            recentTransactions[i].date.year == weekDay.year) {
+              totalSum += recentTransactions[i].amount;
+            }
+      }
+
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+
+
+      return {
+        'day': DateFormat.E(weekDay),
+        'amount': totalSum
+      };
+    });
   }
 
 
@@ -12,7 +39,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: <Widget>[
+
+        ],
       ),
     );
   }
